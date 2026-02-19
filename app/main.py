b52f2dfe26b494bef1d3fdac584107f1c788e73e
dimashabdyken <abdykendinmuhammed@gmail.com>
2026-02-19 21:14:12 +0500

    Update: one string was removed

--- app/main.py
+++ app/main.py
@@ -2,9 +2,7 @@ import uvicorn
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
-from . import crud, database, schemas, models
-
-models.Base.metadata.create_all(bind=database.engine)
+from . import crud, database, schemas
 
 app = FastAPI(title="DevOps ToDo API")
 
@@ -39,13 +37,16 @@ def delete_task(task_id: int, db: Session = Depends(database.get_db)):
 
 
 @app.patch("/tasks/{task_id}", response_model=schemas.TaskResponse)
-def update_task(task_id: int, task_update: schemas.TaskUpdate, db: Session = Depends(database.get_db)):
+def update_task(
+    task_id: int,
+    task_update: schemas.TaskUpdate,
+    db: Session = Depends(database.get_db),
+):
     db_task = crud.update_task(db, task_id=task_id, task_update=task_update)
     if db_task is None:
         raise HTTPException(status_code=404, detail="Task not found")
     return db_task
 
+
 if __name__ == "__main__":
     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
-
-
